a726f58f62ee381c0557dae727b246175f294edd
Pedro Moura Dos Santos <pmouradsant0s@proton.me>
2024-11-02 18:04:31 -0300

    Define as propriedades do produto e métodos para gerenciar produtos.

---
+++ src/models/productModel.ts
@@ -0,0 +1,22 @@
+import connection from '../config/database';
+
+interface Product {
+  id?: number;
+  nome: string;
+  descricao: string;
+  imagem: string;
+  valor: number;
+  quantidade: number;
+}
+
+const createProduct = (product: Product) => {
+  return new Promise((resolve, reject) => {
+    const query = 'INSERT INTO products (nome, descricao, imagem, valor, quantidade) VALUES (?, ?, ?, ?, ?)';
+    connection.query(query, [product.nome, product.descricao, product.imagem, product.valor, product.quantidade], (err, result) => {
+      if (err) reject(err);
+      resolve(result);
+    });
+  });
+};
+
+export { createProduct };
